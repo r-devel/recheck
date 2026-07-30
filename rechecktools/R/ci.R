e8dc2c8fc778693e48a695ac3386294aa6f9377e
rhijmans <r.hijmans@gmail.com>
2026-07-30 03:50:59 -0700

    split revdeps with regex pattern

--- rechecktools/R/ci.R
+++ rechecktools/R/ci.R
@@ -5,7 +5,15 @@
 #' @export
 #' @param path path to source package
 #' @param which either be strong or most
-install_recheck_deps <- function(path = '.', which = 'strong', check_bioc = FALSE){
+#' @param check_bioc also include reverse dependencies from Bioconductor
+#' @param pattern optional regular expression (case insensitive) to subset the
+#' reverse dependencies by name, e.g. `'^[a-k]'`. This can be used to split a
+#' very large reverse dependency check into multiple smaller runs.
+#' @return the names of the (subset of) reverse dependencies for which the
+#' dependencies were installed, invisibly. This can be passed as the `reverse`
+#' argument of [tools::check_packages_in_dir] to check exactly these packages.
+
+install_recheck_deps <- function(path = '.', which = 'strong', check_bioc = FALSE, pattern = ''){
   desc <- as.data.frame(read.dcf(file.path(path, 'DESCRIPTION')))
   pkg <- desc$Package
   repos <- c(
@@ -13,11 +21,16 @@ install_recheck_deps <- function(path = '.', which = 'strong', check_bioc = FALS
     BIOC = if(isTRUE(check_bioc)) 'https://bioconductor.posit.co/packages/devel/bioc'
   )
   db <- utils::available.packages(repos = repos)
-  revdeps <- tools::package_dependencies(pkg, db = db, which = which, reverse = TRUE)[[pkg]]
+  revdeps <- as.character(tools::package_dependencies(pkg, db = db, which = which, reverse = TRUE)[[pkg]])
+  if(nchar(pattern)){
+    revdeps <- grep(pattern, revdeps, ignore.case = TRUE, value = TRUE)
+    message(sprintf("Subsetting reverse dependencies matching '%s': %d packages", pattern, length(revdeps)))
+  }
   packages <- setdiff(unique(c(desc_deps(desc), revdeps)), basepkgs())
   utils::install.packages(packages, dependencies = TRUE)
   #deps <- unique(unlist(unname(tools::package_dependencies(packages, recursive = TRUE))))
   #update.packages(oldPkgs = deps, ask = FALSE)
+  invisible(revdeps)
 }
 
 desc_deps <- function(desc){
